d76a6cb25c0f4353a709d0867b16daac6385b26d
Andrejs Ivanovs <ai00251@rvt.lv>
2024-10-18 10:53:10 +0300

    1810.3

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ document.getElementById('theme-toggle').addEventListener('click', function() {
     });
 });
 
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName("modal");
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target === modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
+
